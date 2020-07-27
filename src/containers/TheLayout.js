f051eef4512263d4bf707a08cc23a30c434bf6d0
nicodev-git <schaefernicker@gmail.com>
2020-07-28 03:29:05 +0800

    store page ui

--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -4,12 +4,14 @@ import { TheContent, TheSidebar, TheFooter, TheHeader } from "./index"
 const TheLayout = () => {
   return (
     <div className="c-app c-default-layout">
+      <div className="top-bg" />
       <div className="c-wrapper">
         <TheHeader />
         <div className="c-body">
           <TheContent />
         </div>
         <TheFooter />
+        <div className="bottom-bg" />
       </div>
     </div>
   )
